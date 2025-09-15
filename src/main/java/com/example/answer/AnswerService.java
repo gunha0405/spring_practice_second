fdfabf3193096f923a31646566b05e04a7f42388
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-15 15:19:37 +0900

    [Feat] chapter 3-09 수정과 삭제 기능

--- src/main/java/com/example/answer/AnswerService.java
+++ src/main/java/com/example/answer/AnswerService.java
@@ -3,10 +3,12 @@ package com.example.answer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import com.example.DataNotFoundException;
 import com.example.question.Question;
 import com.example.user.SiteUser;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,4 +25,24 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
     }
+    
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+    
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
+    }
+    
 }
