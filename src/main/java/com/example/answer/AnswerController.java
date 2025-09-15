b301e1b74c34e472caec63a226a60a00d1a0bd09
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-15 11:26:56 +0900

    [Feat] chapter 2-16 질문 등록 기능 추가하기

--- src/main/java/com/example/answer/AnswerController.java
+++ src/main/java/com/example/answer/AnswerController.java
@@ -4,6 +4,7 @@ package com.example.answer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.question.Question;
 import com.example.question.QuestionService;
+
+import jakarta.validation.Valid;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -21,9 +24,14 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, 
+            @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
