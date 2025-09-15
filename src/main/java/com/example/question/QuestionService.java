3429189af0a92593de1c74fcc3d047b5a56ba147
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-15 10:39:51 +0900

    [Feat] chapter 2-10 상세 페이지 만들기

--- src/main/java/com/example/question/QuestionService.java
+++ src/main/java/com/example/question/QuestionService.java
@@ -1,18 +1,33 @@
 package com.example.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.DataNotFoundException;
+import com.example.answer.AnswerRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
 
+    private final AnswerRepository answerRepository;
+
     private final QuestionRepository questionRepository;
+
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+    
+    public Question getQuestion(Integer id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
