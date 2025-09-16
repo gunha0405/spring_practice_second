2f4ee1e1accf8158ceccc839c3fa08843de269c5
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-16 17:05:00 +0900

    [Feat] chapter 3-14-6 최근 답변, 최근 댓글 순 필터링

--- src/main/java/com/example/question/QuestionRepository.java
+++ src/main/java/com/example/question/QuestionRepository.java
@@ -16,20 +16,29 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	List<Question> findBySubjectLike(String subject);
 	
 	Page<Question> findAll(Pageable pageable);
-	
-	@Query("select "
-            + "distinct q "
-            + "from Question q " 
-            + "left outer join SiteUser u1 on q.author=u1 "
-            + "left outer join Answer a on a.question=q "
-            + "left outer join SiteUser u2 on a.author=u2 "
-            + "where "
-            + "   q.subject like %:kw% "
-            + "   or q.content like %:kw% "
-            + "   or u1.username like %:kw% "
-            + "   or a.content like %:kw% "
-            + "   or u2.username like %:kw% ")
-    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
+
+	@Query("""
+		    SELECT q FROM Question q
+		    WHERE q.subject LIKE %:kw% OR q.content LIKE %:kw%
+		    ORDER BY q.createDate DESC
+		""")      
+    Page<Question> findByKeyword(@Param("kw") String kw, Pageable pageable);
+
+    @Query("""
+    	    SELECT q FROM Question q
+    	    LEFT JOIN q.answerList a
+    	    GROUP BY q
+    	    ORDER BY MAX(a.createDate) DESC NULLS LAST
+    	""")
+    Page<Question> findQuestionsOrderByLatestAnswer(Pageable pageable);
+
+    @Query("""
+    	    SELECT q FROM Question q
+    	    LEFT JOIN q.commentList c
+    	    GROUP BY q
+    	    ORDER BY MAX(c.createDate) DESC NULLS LAST
+    	""")
+    Page<Question> findQuestionsOrderByLatestComment(Pageable pageable);
 	
     Page<Question> findBySubjectAndKeyword(String subject, String keyword, Pageable pageable);
     
--- src/main/java/com/example/question/QuestionService.java
+++ src/main/java/com/example/question/QuestionService.java
@@ -78,11 +78,16 @@ public class QuestionService {
         this.questionRepository.save(q);
     }
     
-    public Page<Question> getList(int page, String kw) {
-        List<Sort.Order> sorts = new ArrayList<>();
-        sorts.add(Sort.Order.desc("createDate"));
-        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAllByKeyword(kw, pageable);
+    public Page<Question> getList(int page, String kw, String filter) {
+        Pageable pageable = PageRequest.of(page, 10);
+
+        if ("answer".equals(filter)) {
+            return questionRepository.findQuestionsOrderByLatestAnswer(pageable);
+        } else if ("comment".equals(filter)) {
+            return questionRepository.findQuestionsOrderByLatestComment(pageable);
+        } else {
+            return questionRepository.findByKeyword(kw, pageable);
+        }
     }
     
     public void modify(Question question, String subject, String content, String value, Category category) {
